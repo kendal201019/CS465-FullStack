77200b058ff1ab4e63e427189166c35e1635bb19
Kendal Guizado <kendal201019@gmail.com>
2024-08-16 23:19:30 -0700

    Module 6 completed baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -52,7 +52,82 @@ const tripsFindByCode = async(req, res) => {
 
 };
 
+// POST: /trips - Adds a new trip
+// Regardless of outcome, response must include html status code
+// and JSON message to requesting clinet
+const tripsAddTrip = async(req, res) => {
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    });
+
+    const q = await newTrip.save();
+
+        if(!q)
+        { // Database returned no data
+            return res
+                .status(400)
+                .json(err);
+        } else { //Return new trip
+            return res
+                .status(201)
+                .json(q);
+        }
+
+        //Uncomment the following kline to show results of operation
+        // on the console
+        // console.long(q);
+
+}
+
+//Put: /trips/tripCode - adds a new trip
+// regardless of outcome, response must include html status code
+// add json message to the requesting client 
+const tripsUpdateTrip = async(req, res) => {
+    //uncomment for debugging
+    console.log(req.params);
+    console.log(req.body);
+
+    const q = await Model
+        .findOneAndUpdate(
+            {'code' : req.params.tripCode},
+            {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            }
+        )
+        .exec();
+
+        if(!q)
+        { // Database returned no data
+            return res
+                .status(400)
+                .json(err);
+        } else { //Return resulting updated trip
+            return res
+                .status(201)
+                .json(q);
+        }
+        // Uncomment the following line to show results operation
+        // on the console
+        //console.log(q);
+};
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,13 +1,20 @@
 const express = require('express'); // Express app
 const router = express.Router(); //Router logic
 
-// This is where we import the controllers we will route
-//const tripsController = require('../controllers/trips');
-const {tripsList} = require("../controllers/trips"); //code change from online
+// This is where we import the controllers we will route 
+const tripsController = require('../controllers/trips');
+//const {tripsList} = require("../controllers/trips"); //code change from online
 // define route for our trips endpoint 
-/*router
+router
+    .route('/trips')
+    .get(tripsController.tripsList) // GET Method routes tripsList
+    .post(tripsController.tripsAddTrip); // Post Method Adds a trip
+
+// GET Method routes tripsFindByCode - requires paramater 
+// PUT Method routes tripsUpdateTrip - requires paramater
+router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode); // GET Method routes tripsList
-*/
-router.get("/trips", tripsList); //GET:
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
+
 module.exports = router;
